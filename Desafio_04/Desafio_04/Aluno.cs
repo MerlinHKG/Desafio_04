0d704ef884ddac40fa637a291120172ecc12dddc
MerlinHKG <orlandomerlin21@gmail.com>
2021-10-10 05:34:04 -0300

    Desafio 04 concluido

---
+++ Desafio_04/Desafio_04/Aluno.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio_04
+{
+    public class Aluno
+    {
+        private string nome {get; set;}
+        private string idade { get; set; }
+        private decimal nota { get; set; }
+
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+
+
+        public void SetIdade(string idade)
+        {
+            this.idade = idade;
+        }
+
+        public string GetIdade()
+        {
+            return idade;
+        }
+
+
+        public void SetNota(decimal nota)
+        {
+            this.nota = nota;
+        }
+
+        public decimal GetNota()
+        {
+            return nota;
+        }
+
+    }
+}
---
+++ Desafio_04/Desafio_04/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Desafio_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Aluno> ListAlunos = new List<Aluno>();
+            var alunos = 0;
+
+            Console.WriteLine("Qual a quantidade de alunos ?");
+            alunos = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            string[] StrAlunos = new string[alunos];
+            string[] Idade = new string[alunos];
+            decimal[] notas = new decimal[alunos];
+            decimal aux = 0;
+
+            for (int i = 0; i < alunos; i++)
+            {
+                var objaluno = new Aluno();
+                Console.WriteLine("Qual o nome do aluno?: ");
+                objaluno.SetNome(Console.ReadLine());
+                Console.WriteLine("Qual a idade do aluno?: ");
+                objaluno.SetIdade(Console.ReadLine());
+                Console.WriteLine("Qual a nota do aluno?: ");
+                objaluno.SetNota(decimal.Parse(Console.ReadLine()));
+                notas[i] = objaluno.GetNota();
+                ListAlunos.Add(objaluno);
+                aux += notas[i];
+            }
+            ListAlunos.ForEach(Alunos => Console.WriteLine("Nome: " + Alunos.GetNome() + ",  " + Alunos.GetIdade() + ", Nota: " + Alunos.GetNota()));
+            Console.WriteLine("A soma das notas é: " + aux);
+
+
+
+        }
+    }
+}
